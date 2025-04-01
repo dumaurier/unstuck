7f3339018a73fb5068842db1561763cc9c96a16d
Josh Vogt <joshvogt@gmail.com>
2025-04-01 12:49:35 -0400

    Clean

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -5,30 +5,11 @@ export default {
       url: '/about/'
     },
     {
-      text: 'Docs',
-      url: '/get-started/'
-    },
-    {
-      text: 'Built with',
-      url: '/built-with/'
-    },
-    {
       text: 'Blog',
       url: '/blog/'
     }
   ],
   bottom: [
-    {
-      text: 'Style guide',
-      url: '/styleguide/'
-    },
-    {
-      text: 'Imprint',
-      url: '/imprint/'
-    },
-    {
-      text: 'Privacy',
-      url: '/privacy/'
-    }
+
   ]
 };
